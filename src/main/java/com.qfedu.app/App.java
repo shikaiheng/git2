ee4100663db1ccdf0b049208319b4ec12ce22f37
kaiheng <874716257@qq.com>
2019-06-14 21:24:28 +0800

    Initial commit

---
+++ src/main/java/com.qfedu.app/App.java
@@ -0,0 +1,10 @@
+package com.qfedu.app;
+
+/**
+ * Created by Administrator on 2019/6/14.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}
